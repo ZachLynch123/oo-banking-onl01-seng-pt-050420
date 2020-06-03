c396abb03171b343f0c42a7b70212dad0e69f3b7
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-06-03 19:41:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,9 @@ class Transfer
   end
   
   def execute_transaction
-    
+    if valid?
+      @sender.balance = @sender.balance - amount
+      @receiver.balance = @receiver.balance + amount
     
   end
   
